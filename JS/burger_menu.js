d6e430dd09eb91fb322daede3504eec0c73bf0b9
JulianGabinAtienza <j.atienza@ecole-ipssi.net>
2024-03-07 09:23:36 +0100

    responsive

--- JS/burger_menu.js
+++ JS/burger_menu.js
@@ -1,9 +1,22 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const burgerMenu = document.getElementById("burger_menu");
-    const menu = document.getElementById("menu");
+// Get the burger menu button and the menu
+const burger_menu = document.querySelector('#burger_menu');
+const menu = document.querySelector('#menu');
 
-    burgerMenu.addEventListener("click", function () {
-        menu.style.display = "block";
-        burger_menu.style.display = "block";
+// Add event listener to the burger menu button
+burger_menu.addEventListener('click', () => {
+    // Toggle the 'active' class on the menu
+    if (menu.style.display = 'none') {
+        menu.style.display = 'flex';
+        menu.style.flexDirection = 'column';
+        burger_menu.style.display = 'none';
+    }
+    
+    document.addEventListener('click', (event) => {
+        // Check if the click occurred outside of the menu
+        if (!menu.contains(event.target) && event.target !== burger_menu) {
+            // Hide the menu
+            menu.style.display = 'none';
+            burger_menu.style.display = 'block';
+        }
     });
 });
