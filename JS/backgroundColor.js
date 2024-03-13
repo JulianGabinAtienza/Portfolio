7554829cca908edbbab6f387b7823dd1e8023f07
JulianGabinAtienza <j.atienza@ecole-ipssi.net>
2024-03-13 17:37:31 +0100

    update background feature :memo:

--- JS/backgroundColor.js
+++ JS/backgroundColor.js
@@ -12,9 +12,22 @@ body.style.background = `linear-gradient(to right, ${leftColor}, ${rightColor})`
 left.addEventListener('input', () => {
   body.style.background = `linear-gradient(to right, ${left.value}, ${right.value})`;
   left.style.border = `3px solid ${left.value}`;
+  if (left.value === '#ffffff') {
+    left.style.color = '#000000';
+} else if (left.value === '#000000') {
+    left.style.color = '#ffffff';
+} else {
+    left.style.border = `3px solid ${left.value}`;
+}
 });
 
 right.addEventListener('input', () => {
     body.style.background = `linear-gradient(to right, ${left.value}, ${right.value})`;
-    right.style.border = `3px solid ${right.value}`;
+    if (right.value === '#ffffff') {
+        right.style.color = '#000000';
+    } else if (right.value === '#000000') {
+        right.style.color = '#ffffff';
+    } else {
+        right.style.border = `3px solid ${right.value}`;
+    }
 });
